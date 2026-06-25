83ce764411b5f3578bbdd3f2fbe513fe80d1126a
rfqrenaldo <renaldorifqi@gmail.com>
2026-06-25 11:17:15 +0700

    Fix typo in Web App URL

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.KKN_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbxVfyEj6a6ZiJLOkR271WEuHHNhr3jb1Q2N-KFsRlfiA3KV7w9a_J70Vf8v8eIvCm09/exec';
+window.KKN_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbxVfyEj6a6ZiJLOkR271WEuHHNhr3jb1Q2N-KFsRlfia3KV7w9a_J70Vf8v8elvCm09/exec';
