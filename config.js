bc6ba78f1cd1a42a39926c293500fdbdcac72df7
rfqrenaldo <renaldorifqi@gmail.com>
2026-06-25 11:30:30 +0700

    chore: update KKN_BACKEND_URL to new deployment endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.KKN_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwvkYuA9LgvSRrJMOIMC8YhSeN2e69A7XOUQIUxi2Uoqw8huKDXe2L4CtSKv1WXcrr5/exec';
+window.KKN_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbw9bCPicWsJRakj7n8hHHXvMIVJPQ8nKf_MR6BEF-pPZNkRoEdjoImFXF503aXps2pJ/exec';
